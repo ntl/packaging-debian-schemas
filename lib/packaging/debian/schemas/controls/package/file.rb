594a54f08a8c9b14a18624b994da4e9b3d55b901
Nathan Ladd <nathanladd+github@gmail.com>
2018-04-30 12:24:32 -0500

    Package::File control includes size and sha256

--- lib/packaging/debian/schemas/controls/package/file.rb
+++ lib/packaging/debian/schemas/controls/package/file.rb
@@ -12,6 +12,14 @@ module Packaging
               "#{Data.package}-#{Data.version}.deb"
             end
 
+            def self.size
+              ::File.size(example)
+            end
+
+            def self.sha256
+              '1685f490558ec3ca1fd43bbfe39c822b0035eed85eed615d724e8505492d1f0e'
+            end
+
             module Malformed
               def self.example
                 ::File.join(__dir__, 'file', basename)
